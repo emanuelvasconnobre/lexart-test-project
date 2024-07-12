68ffa42e4fdd5073ae7aaf77f68006b5cddebaa9
emanuelvasconnobre <emanuelvanobre@gmail.com>
2024-07-12 08:23:30 -0300

    feat(backend): adding IsEmail validation decorator

--- backend/src/validation/auth/login.dto.ts
+++ backend/src/validation/auth/login.dto.ts
@@ -1,7 +1,7 @@
-import { CustomIsString } from "validation/protocols/decorators";
+import { CustomIsEmail, CustomIsString } from "validation/protocols/decorators";
 
 export class LoginDto {
-  @CustomIsString()
+  @CustomIsEmail()
   email!: string;
 
   @CustomIsString()
