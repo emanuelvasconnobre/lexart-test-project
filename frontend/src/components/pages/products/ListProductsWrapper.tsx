95e9e1c78217229261fdf508b5ca4f6884ee981b
emanuelvasconnobre <emanuelvanobre@gmail.com>
2024-07-14 19:47:40 -0300

    refac(frontend): list product add button link

--- frontend/src/components/pages/products/ListProductsWrapper.tsx
+++ frontend/src/components/pages/products/ListProductsWrapper.tsx
@@ -47,7 +47,7 @@ export const ListProductsWrapper = () => {
 
     return <div className="px-5 py-6">
         <div className="py-3">
-            <Link to={"/product/edit/20"}>
+            <Link to={"/product/add"}>
                 <AppButton>
                     Adicionar
                 </AppButton>
