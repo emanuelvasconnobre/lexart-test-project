9e5d1e8b94eb70bab955062031940caf7d80e7f3
emanuelvasconnobre <emanuelvanobre@gmail.com>
2024-07-15 10:44:28 -0300

    rollback(backend): vercel setup

--- backend/dist/domain/models/product.model.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ProductModel = void 0;
-class ProductModel {
-}
-exports.ProductModel = ProductModel;
--- backend/dist/domain/models/user.model.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserModel = void 0;
-class UserModel {
-}
-exports.UserModel = UserModel;
