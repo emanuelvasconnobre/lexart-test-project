5d655e9c9884a55191211828accbe754c0408f00
emanuelvasconnobre <emanuelvanobre@gmail.com>
2024-07-15 09:40:31 -0300

    refac(backend): sequelize using database_url

--- backend/src/config/sequelize.ts
+++ backend/src/config/sequelize.ts
@@ -2,14 +2,8 @@ import { Product, ProductDeleted, User } from "@modules/data/entities";
 import { Sequelize } from "sequelize-typescript";
 import { logger } from "./winston";
 
-export const sequelize = new Sequelize({
-  ssl: process.env.DB_SSL === "true",
-  database: process.env.DB_DATABASE,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
+export const sequelize = new Sequelize(process.env.POSTGRES_URL, {
   dialect: "postgres",
-  port: parseInt(process.env.DB_PORT || "6500"),
   models: [Product, ProductDeleted, User],
   logging:
     process.env["NODE_ENV"] === "dev"
