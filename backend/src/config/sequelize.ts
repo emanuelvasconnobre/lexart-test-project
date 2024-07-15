908c50fdf8d1dcc5d83bce93272cd63c16bde424
emanuelvasconnobre <emanuelvanobre@gmail.com>
2024-07-15 09:27:00 -0300

    refac(backend): adding ssl connection

--- backend/src/config/sequelize.ts
+++ backend/src/config/sequelize.ts
@@ -3,6 +3,7 @@ import { Sequelize } from "sequelize-typescript";
 import { logger } from "./winston";
 
 export const sequelize = new Sequelize({
+  ssl: process.env.DB_SSL === "true",
   database: process.env.DB_DATABASE,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
